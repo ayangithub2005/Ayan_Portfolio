64f7cecf98a0fc3b9b4ef2813fe039866fa35cf4
Ayan Kuila <workwithayankuila@gmail.com>
2026-05-16 20:17:10 +0530

    Fix LinkedIn icon in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -37,7 +37,7 @@ function Footer() {
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                <FaLinkedinIn />
+               <AiFillLinkedin />
               </a>
             </li>
           </ul>
