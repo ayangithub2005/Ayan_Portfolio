e83a967e56d69f02b2815a1ac10292bf50590bb5
Ayan Kuila <workwithayankuila@gmail.com>
2026-05-17 01:50:38 +0530

    Added resume PDF

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -5,6 +5,7 @@ import Particle from "../Particle";
 import Resumecontent from "./ResumeContent";
 import { AiOutlineDownload } from "react-icons/ai";
 import ReactGA from "react-ga";
+import pdf from "../../Assets/Ayan_Kuila_CV.pdf";
 
 function Resume() {
   useEffect(() => {
@@ -19,7 +20,7 @@ function Resume() {
 
         {/* Download Button Top */}
         <Row style={{ justifyContent: "center", position: "relative" }}>
-          <Button variant="primary" href="#" target="_blank">
+          <Button variant="primary" href={pdf} target="_blank">
             <AiOutlineDownload />
             &nbsp;Download CV
           </Button>
@@ -32,8 +33,6 @@ function Resume() {
           <Col md={8} className="resume-right">
 
             <h3 className="resume-title">Education</h3>
-
-            {/* BTECH */}
 
             <Resumecontent
               title="Bachelor of Technology: Computer Science & Engineering [Techno Main Salt Lake]"
@@ -48,8 +47,6 @@ function Resume() {
               ]}
             />
 
-            {/* CLASS 12 */}
-
             <Resumecontent
               title="12th - Higher Secondary (WBCHSE)"
               date="2023"
@@ -59,8 +56,6 @@ function Resume() {
                 "Completed Higher Secondary education with Science background"
               ]}
             />
-
-            {/* CLASS 10 */}
 
             <Resumecontent
               title="10th - Secondary Education (WBBSE)"
@@ -76,9 +71,8 @@ function Resume() {
         </Row>
 
         {/* Download Button Bottom */}
-
         <Row style={{ justifyContent: "center", position: "relative" }}>
-          <Button variant="primary" href="#" target="_blank">
+          <Button variant="primary" href={pdf} target="_blank">
             <AiOutlineDownload />
             &nbsp;Download CV
           </Button>
